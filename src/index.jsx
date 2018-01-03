09ca5886306a0e6ffa8edf40435d724cdf2ee313
SASHA <sanya04588@gmail.com>
2018-01-03 16:16:59 +0200

    Modified

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const API_KEY = 'AIzaSyBpwXBeC0MKZVZ1E9c2hfR4YF5chx1P5tU';
+const App = () => {
+  return <div>Hi</div>
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
+
